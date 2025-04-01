d3ee3d1fec97bb9e773d7483abafe544a1e4db48
Aneetam <88475279+Aneetam@users.noreply.github.com>
2025-04-01 13:18:48 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,41 @@
+let counterElement = document.getElementById("counterValue");
+console.log((counterElement).outerHTML);
+
+function onDecrement() {
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) - 1;
+    counterElement.textContent = updatedCounterValue;
+
+    if (updatedCounterValue > 0) {
+        counterElement.style.color = "green";
+    } else if (updatedCounterValue < 0) {
+        counterElement.style.color = "red";
+    } else {
+        counterElement.style.color = "black";
+    }
+
+}
+
+function onIncrement() {
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+    counterElement.textContent = updatedCounterValue;
+
+    if (updatedCounterValue > 0) {
+        counterElement.style.color = "green";
+    } else if (updatedCounterValue < 0) {
+        counterElement.style.color = "red";
+    } else {
+        counterElement.style.color = "black";
+    }
+
+
+
+}
+
+function onReset() {
+    let updatedCounterValue = 0;
+    counterElement.textContent = updatedCounterValue;
+    counterElement.style.color = "black";
+
+}
