13323cb6926e0ff07ca5db6a93148f130cf94457
Aneetam <88475279+Aneetam@users.noreply.github.com>
2025-06-11 18:04:42 +0530

    Add files via upload

---
+++ Splice Playground.js
@@ -0,0 +1,42 @@
+let arr = [1, 7, 3, 1, 0, 20, 77];
+
+let startIndexInputE1 = document.getElementById("startIndexInput");
+let deleteCountInputE1 = document.getElementById("deleteCountInput");
+let itemToAddInputE1 = document.getElementById("itemToAddInput");
+let updatedArrayE1 = document.getElementById("updatedArray");
+let spliceBtnE1 = document.getElementById("spliceBtn");
+
+function convertArrToJSONStringAndAppend() {
+    let stringifiedArr = JSON.stringify(arr);
+    updatedArrayE1.textContent = stringifiedArr;
+
+}
+
+convertArrToJSONStringAndAppend();
+spliceBtnE1.onclick = function spliceArray() {
+    let startIndex = startIndexInputE1.value;
+    let deleteCount = deleteCountInputE1.value;
+    let itemToAdd = itemToAddInputE1.value;
+
+    if (startIndex === "") {
+        alert("Please enter start index");
+        return;
+    }
+
+    if (deleteCount === "") {
+        deleteCount = 0;
+    }
+
+    if (itemToAdd === "") {
+        arr.splice(parseInt(startIndex), parseInt(deleteCount));
+    } else {
+        arr.splice(parseInt(startIndex), parseInt(deleteCount), itemToAdd);
+    }
+
+    startIndexInputE1.value = "";
+    deleteCountInputE1.value = "";
+    itemToAddInputE1.value = "";
+    convertArrToJSONStringAndAppend();
+
+
+};
